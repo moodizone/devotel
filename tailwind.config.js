c27a3fb832a58015cf7de62d644f92e69c14fdb6
moodi <moodizone@gmail.com>
2025-06-06 18:39:56 +0330

    refactor Tailwind CSS configuration and remove unused color extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
-import { zinc } from 'tailwindcss/colors'
-
 export default {
   darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        gray: zinc,
-      },
-    },
-  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
