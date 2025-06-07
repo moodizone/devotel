914e9db95cae18f004113b874a43c4ffbd9cbf8e
moodi <moodizone@gmail.com>
2025-06-07 21:42:08 +0330

    add fetchForms and corresponding types

--- src/services/forms.ts
+++ src/services/forms.ts
@@ -1,44 +1,82 @@
 import { fetchAPI } from '../utils/fetchAPI';
 
-export type FieldType = 'text' | 'number' | 'date' | 'select' | 'radio' | 'checkbox' | 'group';
-
-export interface Validation {
-  min?: number;
-  max?: number;
-  pattern?: string;
-}
-
-export interface DynamicOptions {
-  dependsOn: string;
-  endpoint: string;
-  method: string;
-}
-
-export interface Visibility {
-  dependsOn: string;
-  condition: 'equals';
-  value: string;
-}
-
-export interface FormField {
+//================================
+// Common properties
+//================================
+export interface BaseFormField {
   id: string;
   label: string;
-  type: FieldType;
-  required: boolean;
-  options?: string[];
-  validation?: Validation;
-  dynamicOptions?: DynamicOptions;
-  visibility?: Visibility;
-  fields?: FormField[];
+  required?: boolean;
+  visibility?: {
+    dependsOn: BaseFormField["id"];
+    condition: "equals";
+    value: string;
+  };
 }
+
+//================================
+// Specific properties
+//================================
+export interface TextInput extends BaseFormField {
+  type: "text";
+  validation?: {
+    pattern?: string;
+  };
+}
+
+export interface NumberInput extends BaseFormField {
+  type: "number";
+  validation?: {
+    min?: number;
+    max?: number;
+  };
+}
+
+export interface RadioInput extends BaseFormField {
+  type: "radio";
+  options: string[];
+}
+
+export interface CheckboxInput extends BaseFormField {
+  type: "checkbox";
+  options: string[];
+}
+
+export interface DateInput extends BaseFormField {
+  type: "date";
+}
+
+export interface SelectInput extends BaseFormField {
+  type: "select";
+  options?: string[];
+  dynamicOptions?: {
+    dependsOn: BaseFormField["id"];
+    endpoint: string;
+    method: "GET";
+  };
+}
+
+export interface GroupInput extends BaseFormField {
+  type: "group";
+  fields: FormFieldType[];
+}
+
+export type FormFieldType =
+  | TextInput
+  | NumberInput
+  | RadioInput
+  | CheckboxInput
+  | DateInput
+  | GroupInput
+  | SelectInput;
 
 export interface Form {
   formId: string;
   title: string;
-  fields: FormField[];
+  fields: FormFieldType[];
 }
 
 export async function fetchForms(): Promise<Form[]> {
   const response = await fetchAPI<Form[]>('/insurance/forms');
   return response;
-} 
+}
